0d67c012be48816459fb962a1850130e66b3a7ff
sist21 <sist21@DESKTOP-OAFQCCT>
2020-10-15 10:01:55 +0900

    10-15 10:01  dayeun

---
+++ greentable_admin/src/kr/co/greentable/admin/domain/SelectAskDetailDomain.java
@@ -0,0 +1,30 @@
+package kr.co.greentable.admin.domain;
+
+public class SelectAskDetailDomain {
+	public String ask_subject, ask_content, ask_answer;
+
+	public String getAsk_subject() {
+		return ask_subject;
+	}
+
+	public void setAsk_subject(String ask_subject) {
+		this.ask_subject = ask_subject;
+	}
+
+	public String getAsk_content() {
+		return ask_content;
+	}
+
+	public void setAsk_content(String ask_content) {
+		this.ask_content = ask_content;
+	}
+
+	public String getAsk_answer() {
+		return ask_answer;
+	}
+
+	public void setAsk_answer(String ask_answer) {
+		this.ask_answer = ask_answer;
+	}
+	
+}//class 
---
+++ greentable_admin/src/kr/co/greentable/admin/vo/UpdateAnswerVO.java
@@ -0,0 +1,22 @@
+package kr.co.greentable.admin.vo;
+
+public class UpdateAnswerVO {
+	public String ask_num, ask_answer;
+
+	public String getAsk_num() {
+		return ask_num;
+	}
+
+	public void setAsk_num(String ask_num) {
+		this.ask_num = ask_num;
+	}
+
+	public String getAsk_answer() {
+		return ask_answer;
+	}
+
+	public void setAsk_answer(String ask_answer) {
+		this.ask_answer = ask_answer;
+	}
+	
+}//class
